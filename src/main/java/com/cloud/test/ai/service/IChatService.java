c30edf5c2a13354582c87b7d34975c86435153e2
astaguo <asta.guo.cn@gmail.com>
2026-07-21 22:33:25 +0800

    Add some functions

--- src/main/java/com/cloud/test/ai/service/IChatService.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.test.ai.service;
-
-import com.cloud.test.ai.dto.ChatMessageDto;
-import reactor.core.publisher.Flux;
-
-public interface IChatService {
-    Flux<String> sendMessage(ChatMessageDto messageDto);
-}
